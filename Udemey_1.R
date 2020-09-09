5546f55b0de872ca96617701f5ab83aaf6f4b37a
Christina Thompson <can2c33@gmail.com>
2020-09-09 10:34:09 -0500

    This is Udemey code 1.6 (v2)

---
+++ Udemey_1.R
@@ -0,0 +1,10 @@
+a <- 2
+print(a)
+print ('christina')
+
+##this is a vector
+student.height <- c(101, 122, 103)
+##c is an argument
+
+##now I can ask for the mean of the vector student.height
+mean(student.height)
